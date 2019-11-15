92099ca5bcfa96889c50a7a07e9ee73d55842923
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 15:30:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
   male_pigeon_data = {} 
   
   female_pigeon_data.each |name| do 
-    male_pigeon_data[name] = { color: [], gender: [],
+    male_pigeon_data[name] = { color: [], gender: ["female"], lives: [] }
   end 
   
   new_pigeon_data = male_pigeon_data.merge(female_pigeon_data)
