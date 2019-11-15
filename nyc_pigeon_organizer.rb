614d066fdf77de9662e3261790d5619d7f933995
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 15:45:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,7 +18,22 @@ def nyc_pigeon_organizer(data)
     
     pigeons_colors = data[:color]
      
-    new_pigeon_data.each do |
-       
+    new_pigeon_data.each do |name, attribute_hash|
+      pigeons_colors.each do |color, color_array|
+        if color_array.include?(name)
+        new_pigeon_data[name][:color].push(color.to_s)
+       end 
+       end 
     end 
+    
+    location_of_pigeons = data[:lives]
+    
+    new_pigeon_data.each do |name, attribute_hash|
+      location_of_pigeons.each do |location, location_array|
+        if location_array.include?(name)
+          new_pigeon_data[name][:lives].push(location)
+        end 
+      end 
+    end 
+    new_pigeon_data
 end
